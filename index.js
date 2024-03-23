cb6b365877d0fa78b2dfdac8fec9008b37464c73
Naoya <nokamoto@fordham.edu>
2024-03-23 14:33:22 +0900

    changes as of 2024-03-23

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.get('/', (req, res) => {
     res.sendFile(join(__dirname, 'chat_interface.html'));
 });
 
-io.on('connection', (socket) => {
+io.on('connection', async (socket) => {
   socket.on('chat message', async (msg) => {
     let result;
       try {
@@ -47,6 +47,20 @@ io.on('connection', (socket) => {
       // include the offset with the message
       io.emit('chat message', msg, result.lastID);
   });
+
+  if (!socket.recovered) {
+    // if the connection state recovery was not successful
+    try {
+      await db.each('SELECT id, content FROM messages WHERE id > ?',
+        [socket.handshake.auth.serverOffset || 0],
+        (_err, row) => {
+          socket.emit('chat message', row.content, row.id);
+        }
+      )
+    } catch (e) {
+      // something went wrong
+    }
+  }
 });
 
 server.listen(3000, () => {
